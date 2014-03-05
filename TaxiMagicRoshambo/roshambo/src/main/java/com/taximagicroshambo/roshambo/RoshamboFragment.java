fcb704b88a182c0d7a7e6d051a10fe62f9474190
Aaron Huttner <aaron@gryphn.co>
2014-03-04 22:26:52 -0500

    add async task to challenge server

--- TaxiMagicRoshambo/roshambo/src/main/java/com/taximagicroshambo/roshambo/RoshamboFragment.java
+++ TaxiMagicRoshambo/roshambo/src/main/java/com/taximagicroshambo/roshambo/RoshamboFragment.java
@@ -3,48 +3,31 @@ package com.taximagicroshambo.roshambo;
 import android.app.Activity;
 import android.app.Fragment;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.taximagicroshambo.roshambo.services.ThrowToWinAsyncTask;
+
+import java.net.URL;
 
 
-/**
- * A simple {@link android.support.v4.app.Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link RoshamboFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link RoshamboFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
-public class RoshamboFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class RoshamboFragment extends Fragment implements View.OnClickListener{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment RoshamboFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static RoshamboFragment newInstance(String param1, String param2) {
         RoshamboFragment fragment = new RoshamboFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
     public RoshamboFragment() {
@@ -54,24 +37,19 @@ public class RoshamboFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_roshambo, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_roshambo, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        view.findViewById(R.id.rock).setOnClickListener(this);
+        view.findViewById(R.id.paper).setOnClickListener(this);
+        view.findViewById(R.id.scissors).setOnClickListener(this);
+
+        return view;
     }
 
     @Override
@@ -83,12 +61,36 @@ public class RoshamboFragment extends Fragment {
             throw new ClassCastException(activity.toString()
                     + " must implement OnFragmentInteractionListener");
         }
+
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        String choice = "";
+
+        switch(v.getId()){
+            case R.id.rock:
+                choice = "rock";
+                break;
+            case R.id.paper:
+                choice = "paper";
+                break;
+            case R.id.scissors:
+                choice = "scissors";
+                break;
+        }
+
+        Uri.Builder builder = Uri.parse(getString(R.string.server_address)).buildUpon();
+        builder.appendPath(choice);
+
+        new ThrowToWinAsyncTask().execute(builder.build());
+
     }
 
     /**
